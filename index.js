f97bc464c648f38bcbdea467271985de4a68b2bc
Emil Filtenborg Henriksen <emilhenriksen@yahoo.dk>
2017-10-25 01:03:08 +0200

    addidions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/user');
+require('./models/survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveysRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
 	// make sure express serve up production assets
