b7042e58ac7027fd754ed9cd8e4edd7d73641fc8
Emil Filtenborg Henriksen <emilhenriksen@yahoo.dk>
2017-11-03 00:11:22 +0100

    handling sendgrid data

--- routes/surveysRoutes.js
+++ routes/surveysRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -12,7 +15,20 @@ module.exports = app => {
 	});
 
   app.post('/api/surveys/webhooks', (req, res) =>{
-    console.log(req.body);
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    
+    const events = _.chain(req.body)
+      .map(({email, url}) => {
+        const match = p.test(new URL(url).pathname);
+        if(match){
+          return {email, surveyId: match.surveyId, choice:match.choice};
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
+
+    console.log(events);
     res.send({});
   });
 
